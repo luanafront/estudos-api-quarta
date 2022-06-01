b7c369a601d2bc92d504d1de42f2e8946af92792
luanafront <luana.lopes.rodrigues1@gmail.com>
2022-06-01 18:09:00 -0300

    page style and other features

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,43 @@ import Card from './components/card';
 
 function App() {
   const [apiData, setApiData] = useState(null)
+  const [page, setPage] = useState(1)
   
   useEffect(() => {
-    axios.get("https://rickandmortyapi.com/api/character/").then((res) => {
+    axios.get(`https://rickandmortyapi.com/api/character/?page=${page}`).then((res) => {
       let data = res.data
       setApiData(data)
     })
-  }, [])
+  }, [page])
+  
+  const prevPage = () => {
+    if( page > 0){
+      setPage(page - 1)
+    }
+  }
+  const nextPage = () => {
+    setPage(page + 1)
+  }
+  
   return (
     apiData === null ? 
-    <p>Carregando</p> :
+    <p>Loaging</p> :
     <div className="App">
       <header className="App-header">
+      <div className="pageButtons">
+        <button
+          className="pageButtons__button--prev"
+          onClick={prevPage}
+        >
+          Previous
+        </button>
+        <button
+          className="pageButtons__button--next"
+          onClick={nextPage}
+        >
+          Next
+        </button>
+      </div>
         {apiData.results.map((data, index) => {
           return (
             <Card data={data} index={index}/>
